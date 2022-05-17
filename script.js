aede6e0ca7be2373594ca84233dde6d7ac609d2e
Nickolay Dimitrov <dimitrov.nikolai9224@gmail.com>
2022-05-17 22:33:49 +0300

    Carousel, need to update the automatic change.

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-const slides = document.querySelector('.slide')
+const slides = document.querySelectorAll('.slide')
 const dots = document.querySelector('.carousel-buttons')
-console.log(dots)
-let slideIndex = 1;
+// current slide for indicating the count of slides
+let currSlide = 0;
 
-let firstImage = './assests/firstSlide.jpg'
-let secondImage = './assests/secondSlide.jpg'
-let thirdImage = './assests/thirdSlide.jpg'
+let maxSlide = slides.length - 1;
 
-let img = [{
-    src: './assests/firstSlide.jpg',
-    src: './assests/secondSlide.jpg',
-    src: './assests/thirdSlide.jpg'
-}]
+//loop torugh each slide to translateX property * 100%
+slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${indx * 100}%)`;
+});
 
-img.map((img) => {
-    let imgTag = document.createElement('img');
-    imgTag.src = img.src;
+//adding eventlistener to all dots
+dots.addEventListener('click', (e) => {
+    
+    //using event.target to locate the correct slide
+    if(e.target.className === 'slide-selector1') {
+        // indicating the index of the slide. 
+        currSlide = 0
+        // translatingX the slides based on the currSlideIndex, multiplying to 100 translated to 0%.
+        slides.forEach((slide, indx) => {
+            slide.style.transform = `translateX(${indx * 100}%)`;
+        }); 
+    }
 
-    // append the image to the slide wrapper
-    slides.appendChild(imgTag)
+    if(e.target.className === 'slide-selector2' ) {
+        // hard-coding the slide index
+        currSlide = 1
+        // translateX subtracting the index with the currslide, multiply by 100
+        slides.forEach((slide, indx) => {
+            slide.style.transform = `translateX(${100 * (indx - currSlide)}%)`;
+        }); 
+    }
+
+    if(e.target.className === 'slide-selector3' ) {
+        currSlide = 2
+        slides.forEach((slide, indx) => {
+            slide.style.transform = `translateX(${100 * (indx - currSlide)}%)`;
+        }); 
+    }
+
 })
-
-
-
-
-function updateImage() {
-
-
-}
-
-updateImage()
 
 
